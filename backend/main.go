f6b179471fde4f4001da0ed5423346b71483fb35
orkun incili <orkunincili0@gmail.com>
2026-03-11 18:29:50 +0300

    Add loadKubeConfig function for incluster usage

--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,16 @@ import (
 	"log"
 	"net/http"
 
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 
-	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	config, err := loadKubeConfig()
 	if err != nil {
-		log.Fatalf("Config error: %v", err)
+		log.Fatalf("Failed to load Kubernetes config: %v", err)
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
@@ -27,4 +28,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loadKubeConfig() (*rest.Config, error) {
+	config, err := rest.InClusterConfig()
+	if err == nil {
+		log.Println("Using in-cluster Kubernetes config")
+		return config, nil
+	}
+
+	config, err = clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Using local kubeconfig")
+	return config, nil
 }
